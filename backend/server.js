d918da2e8d784496b4e5b4f1c674205aa8abd45e
Amir Khan <amirkhan20212026@gmail.com>
2026-07-16 12:10:26 +0530

    Fix backend deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const contactRoutes = require("./routes/contactRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const galleryRoutes = require("./routes/galleryRoutes");
+const projectRoutes = require("./routes/projectRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use("/api", contactRoutes);
+app.use("/api", adminRoutes);
+app.use("/api", galleryRoutes);
+app.use("/api", projectRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully");
+});
+
 const PORT = process.env.PORT || 5000;
+
+// 
+console.log("PORT =", process.env.PORT);
 
 mongoose
   .connect(process.env.MONGO_URI)
